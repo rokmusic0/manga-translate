0acb221ec905deb1c4ef47f87c6f1c4d598cbf30
Rok Mušič <rokmusic0@gmail.com>
2026-03-28 13:46:13 +0100

    Add support for macos in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
-uv pip install paddlepaddle-gpu==3.3.0 -i https://www.paddlepaddle.org.cn/packages/stable/cu130/
+#!/usr/bin/env bash
+OS=$(uname)
+if [[ "$OS" == "Darwin" ]]; then
+    uv pip install paddlepaddle==3.2.1 -i https://www.paddlepaddle.org.cn/packages/stable/cpu/
+    uv pip install "mlx-vlm>=0.3.11"
+else # Linux
+    uv pip install paddlepaddle-gpu==3.3.0 -i https://www.paddlepaddle.org.cn/packages/stable/cu130/
+    uv pip install "numpy<2.4"
+fi
 uv pip install "paddleocr[doc-parser]"
-uv pip install "numpy<2.4"
