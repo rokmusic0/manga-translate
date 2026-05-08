5149e46244e38e0a3124c7527c51164289d67c49
Rok Mušič <rokmusic0@gmail.com>
2026-05-08 09:26:48 +0200

    Intercept warnings to logger

--- logging_config.py
+++ logging_config.py
@@ -1,6 +1,14 @@
+import logging
 import sys
+import warnings
 
 from loguru import logger
+
+
+class InterceptHandler(logging.Handler):
+    def emit(self, record: logging.LogRecord) -> None:
+        level = record.levelname if record.levelname in logger._core.levels else record.levelno
+        logger.opt(exception=record.exc_info, depth=6).log(level, record.getMessage())
 
 
 def configure_logging(verbose: bool = False) -> None:
@@ -10,3 +18,7 @@ def configure_logging(verbose: bool = False) -> None:
         level="DEBUG" if verbose else "INFO",
         format="<green>{time:HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
     )
+
+    logging.captureWarnings(True)
+    logging.basicConfig(handlers=[InterceptHandler()], level=0, force=True)
+    warnings.simplefilter("default")
